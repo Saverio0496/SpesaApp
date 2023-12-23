731e72d3dac741b53fb0ff3f26fdb477145b7fe3
Saverio <save.carelli@gmail.com>
2023-12-23 03:49:10 +0100

    implementazione firebase e inizio progetto

---
+++ src/environments/environments.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDVjQMoExUMz4XiOEk4L4CkXIrdwD8mrYo",
+    authDomain: "spesaapp-49034.firebaseapp.com",
+    databaseURL: "https://spesaapp-49034-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "spesaapp-49034",
+    storageBucket: "spesaapp-49034.appspot.com",
+    messagingSenderId: "149557047250",
+    appId: "1:149557047250:web:5cf63aa619149b4f02220b",
+    measurementId: "G-P8K8LNLDRG"
+  }
+};
